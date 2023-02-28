390d29637a68c80f67d179b15aa4ba6537781de7
JayCabasag99 <jay@metromart.com>
2023-03-01 01:53:44 +0800

    Added Changes on domain

--- services/serverConfig.ts
+++ services/serverConfig.ts
@@ -1,2 +1,2 @@
-export const CURRENT_SERVER_DOMAIN = 'https://univtraze-web-server-gwluv5z3y-univtraze.vercel.app/api'
+export const CURRENT_SERVER_DOMAIN = 'https://univtraze-web-server.onrender.com/api'
 export const PH_COVID_DATA = 'https://disease.sh/v3/covid-19/countries/PH?strict=true'
