e126d4ee6fac5dbbe10136112ca3124d77bb35af
JayCabasag99 <jay@metromart.com>
2023-03-01 01:43:34 +0800

    Added new API

--- services/serverConfig.ts
+++ services/serverConfig.ts
@@ -1,2 +1,2 @@
-export const CURRENT_SERVER_DOMAIN = 'https://univtraze-web-server-gwluv5z3y-univtraze.vercel.app/api'
+export const CURRENT_SERVER_DOMAIN = 'http://univtrazewebapi-env-1.eba-evg8dpfg.ap-northeast-1.elasticbeanstalk.com/api'
 export const PH_COVID_DATA = 'https://disease.sh/v3/covid-19/countries/PH?strict=true'
