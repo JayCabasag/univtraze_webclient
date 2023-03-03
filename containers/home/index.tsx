a2ab9922b2ebb1e44b7c86b9aeddf13eee974370
JayCabasag99 <jay@metromart.com>
2023-03-03 20:49:30 +0800

    Added padding top

--- containers/home/index.tsx
+++ containers/home/index.tsx
@@ -69,7 +69,7 @@ export default function HomeContainer({ children }: { children: ReactNode}) {
       </nav>
 
       <aside id="logo-sidebar" className={isShowSidebar ? "w-3/4 md:w-64 z-30 fixed shadow-md h-full md:-mt-[3px] md:z-30" : "fixed top-0 left-0 z-30 md:w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 shadow-md"} aria-label="Sidebar">
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <div className="h-full pt-4 px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul className="space-y-2">
               <li>
                   <Link href="/home/dashboard" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
