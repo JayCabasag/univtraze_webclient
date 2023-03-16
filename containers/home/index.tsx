667547504cd0eb7e125cf073009a95dbec67ae94
JayCabasag99 <jay@metromart.com>
2023-03-16 16:30:33 +0800

    Added highlights for nav items

--- containers/home/index.tsx
+++ containers/home/index.tsx
@@ -18,6 +18,7 @@ import { getUidFromToken } from '@/utils/parser';
 export default function HomeContainer({ children, props }: { children: ReactNode, props: PageProps }) {
   const { uid } = getUidFromToken(props.token as string)
   const { type, fullname  } = userStore()
+
   const [isShowSidebar, setIsShowSidebar] = useState(false)
   const [showUserQrCode, setShowUserQrCode] = useState(false)
   const stringifiedQrData = base64.encode(JSON.stringify({id: uid, type: type, name: fullname}))
@@ -25,6 +26,13 @@ export default function HomeContainer({ children, props }: { children: ReactNode
   const isOnDashboardRoute = 
     router.asPath === '/home' || router.asPath === '/home/dashboard' ||
     router.asPath === '/home/emergency-report' || router.asPath === '/home/report-disease'
+
+  const isHomeRoute = router.asPath === '/home' || router.asPath === '/home/dashboard'
+  const isNotificationsRoute = router.asPath === '/home/notifications'
+  const isTemperatureHistoryRoute = router.asPath === '/home/temperature-history'
+  const isUpdateProfileRoute = router.asPath === '/home/update-profile'
+  const isRoomVisitedRoute = router.asPath === '/home/room-visited'
+  const isVaccineInformationRoute = router.asPath === '/home/vaccine-information' 
 
   const handleToggleUserQrCode = () => {
     setShowUserQrCode(prevState => !prevState)
@@ -66,7 +74,7 @@ export default function HomeContainer({ children, props }: { children: ReactNode
                   <div className='flex gap-4 items-center max-w-[180px] md:max-w-md'>
                     <p className='truncate'>Welcome {fullname}</p>
                     <button onClick={handleToggleUserQrCode}>
-                      <img className="w-8 h-8 min-w-min rounded-full" src={IMAGES.DEFAULT_PROFILE_PHOTO} alt="user photo" />
+                      <img className="w-8 h-8 min-w-min rounded-full" src='/qr-code.svg' alt="user photo" />
                     </button>
                   </div>
                 </div>
@@ -79,47 +87,157 @@ export default function HomeContainer({ children, props }: { children: ReactNode
         <div className="h-full pt-4 md:pt-0 px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul className="space-y-2">
               <li>
-                  <Link href="/home/dashboard" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                    <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Dashboard</span>
+                  <Link
+                    href="/home/dashboard"
+                    className={isHomeRoute 
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className={isHomeRoute 
+                        ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                        : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                      }
+                      fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
+                    <span
+                      className={isHomeRoute 
+                        ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                        : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                      }
+                    >
+                      Dashboard
+                    </span>
                   </Link>
               </li>
               <li>
-                  <Link href="/home/notifications" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                    <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Notifications</span>
+                  <Link
+                    href="/home/notifications"
+                    className={isNotificationsRoute
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className={isNotificationsRoute
+                        ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                        : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                      }
+                      fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
+                    <span
+                      className={isNotificationsRoute
+                        ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                        : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                      }
+                    >
+                      Notifications
+                    </span>
                   </Link>
               </li>
               <li>
-                  <Link href="/home/temperature-history" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                  <svg fill="none" className='flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white' stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <Link
+                    href="/home/temperature-history"
+                    className={isTemperatureHistoryRoute
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                  <svg
+                    fill="none"
+                    className={isTemperatureHistoryRoute
+                      ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                      : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                    }
+                    stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Temperature History</span>
+                  <span
+                    className={isTemperatureHistoryRoute
+                      ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                      : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                    }
+                  >Temperature History</span>
                   </Link>
               </li>
               <li>
-                  <Link href="/home/update-profile" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                  <svg fill="none" className="flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <Link 
+                    href="/home/update-profile" 
+                    className={isUpdateProfileRoute
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                  <svg
+                    fill="none"
+                    className={isUpdateProfileRoute
+                      ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                      : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                    }
+                    stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Update profile</span>
+                  <span
+                    className={isUpdateProfileRoute
+                      ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                      : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                    }
+                  >Update profile</span>
                   </Link>
               </li>
               <li>
-                  <Link href="/home/room-visited" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                  <svg fill="none" stroke="currentColor" className='flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white' strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <Link
+                    href="/home/room-visited"
+                    className={isRoomVisitedRoute
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                  <svg 
+                    fill="none"
+                    stroke="currentColor" 
+                    className={isRoomVisitedRoute
+                      ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                      : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                    }
+                    aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Room visited</span>
+                  <span 
+                    className={isRoomVisitedRoute
+                      ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                      : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                    }
+                  >
+                    Room visited
+                  </span>
                   </Link>
               </li>
               <li>
-                  <Link href="/home/vaccine-information" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                  <svg fill="none" className='flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white' stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <Link
+                    href="/home/vaccine-information"
+                    className={isVaccineInformationRoute
+                      ? "bg-main flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700" 
+                      : "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                    }
+                  >
+                  <svg fill="none" 
+                    className={isVaccineInformationRoute
+                      ? "flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-main"
+                      : "flex-shrink-0 w-6 h-6 text-main transition duration-75 dark:text-main group-hover:text-gray-900 dark:group-hover:text-white"
+                    }
+                    stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"></path>
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap font-medium text-main">Vaccine Information</span>
+                  <span
+                    className={isVaccineInformationRoute
+                      ? "flex-1 ml-3 whitespace-nowrap font-medium text-white"
+                      : "flex-1 ml-3 whitespace-nowrap font-medium text-main"
+                    }
+                  >
+                    Vaccine Information
+                  </span>
                   </Link>
               </li>
               <li>
